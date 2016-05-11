f2189207d1ac00116012a1dcd7e7626e814c3078
Aaron Toys <aarontoys@gmail.com>
2016-05-11 10:32:18 -0600

    intial commit

---
+++ Unit-2-Encapsulation/problems/Encapsulation01/Encapsulation01.java
@@ -0,0 +1,14 @@
+package galvanize;
+
+import encapsulation01_lib.Proposal;
+
+public class Encapsulation01 {
+  public static void main(String[] args){
+    Proposal prop = new Proposal();
+    prop.name = "Some thing";
+    System.out.println(prop.name);
+
+    prop.allInFavor = 24;
+    System.out.println(prop.allInFavor);
+  }
+}
