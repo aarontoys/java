c1f663227ef922d25d913ac324f53a4550493590
Aaron Toys <aarontoys@gmail.com>
2016-05-12 11:46:24 -0600

    exercise 2 done

--- Unit-3-Inheritance/problems/Inheritance02/inheritance02_lib/Manager.java
+++ Unit-3-Inheritance/problems/Inheritance02/inheritance02_lib/Manager.java
@@ -1,9 +1,14 @@
-package inheritance02_lib;
+package inheritance02_lib; 
 
-public class Manager {
-  private String status = "employed";
-
-  public String getStatus() {
-    return "This person is " + status;
-  }
+public class Manager extends Employee {
+  // Manager manager = new Manager ();
+  // manager.status = "employed";
+  // manager.getStatus()
 }
+// public class Manager {
+//   private String status = "employed";
+
+//   public String getStatus() {
+//     return "This person is " + status;
+//   }
+// }
